647cfbe0a2d40ec978074e5fd9ad160ef416de66
nardf-iscteiulpt <nardf@iscte-iul.pt>
2017-03-15 21:53:05 +0000

    Teste4

--- src/main/java/mt/client/impl/MicroTraderClientImpl.java
+++ src/main/java/mt/client/impl/MicroTraderClientImpl.java
@@ -10,9 +10,8 @@ import mt.comm.ClientComm;
 import mt.comm.impl.ClientCommImpl;
 
 public class MicroTraderClientImpl implements MicroTraderClient {
+
 	
-//Commit teste
-	//Commit para video final
 	
 	public static void main(String args[]) {
 		ClientComm clientComm = new ClientCommImpl();
