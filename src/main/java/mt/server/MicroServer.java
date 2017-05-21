f79e17d2c1f33b9faaac80ffc9a956ab30d220d9
jmrfa11-iscteiulpt <jmrfa11@iscte-iul.pt>
2017-05-21 20:12:59 +0100

    MiniTrader-EU-1.1 (Final Delivery)

--- src/main/java/mt/server/MicroServer.java
+++ src/main/java/mt/server/MicroServer.java
@@ -28,6 +28,8 @@ import mt.filter.AnalyticsFilter;
  * @author Group 78
  *
  */
+
+//FINAL DELIVERY
 public class MicroServer implements MicroTraderServer {
 
 	public static void main(String[] args) {
